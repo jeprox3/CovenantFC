5a0349bdeb779da3148772987e1e64dc05c6d3b3
Jeff Davis <jeffd@s290.local>
2016-11-15 14:41:41 -0500

    initial setup

---
+++ script.js
@@ -0,0 +1,99 @@
+$(document).ready(function(){
+    $('.cards').click(function(){
+        $('.player-card').fadeIn('slow')   
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $('.player-card').click(function(){
+        $('.player-card').fadeOut('slow')   
+        
+    });
+
+});
+
+
+
+//forward hover//
+
+$(document).ready(function(){
+    $('.forward-hover').mouseover(function(){
+        $('.forward-hover').fadeTo('fast', 0.2)   
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $('.forward-hover').mouseleave(function(){
+        $('.forward-hover').fadeTo('fast', 0)   
+        
+    });
+
+});
+
+
+
+//mid fielder hover//
+
+$(document).ready(function(){
+    $('.mid-hover').mouseover(function(){
+        $('.mid-hover').fadeTo('fast', 0.2)   
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $('.mid-hover').mouseleave(function(){
+        $('.mid-hover').fadeTo('fast', 0)   
+        
+    });
+
+});
+
+
+
+
+
+//defense
+
+$(document).ready(function(){
+    $('.defense-hover').mouseover(function(){
+        $('.defense-hover').fadeTo('fast', 0.2)   
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $('.defense-hover').mouseleave(function(){
+        $('.defense-hover').fadeTo('fast', 0)   
+        
+    });
+
+});
+
+
+
+
+//goalie hover//
+
+$(document).ready(function(){
+    $('.goalie-hover').mouseover(function(){
+        $('.goalie-hover').fadeTo('fast', 0.2)   
+        
+    });
+
+});
+
+$(document).ready(function(){
+    $('.goalie-hover').mouseleave(function(){
+        $('.goalie-hover').fadeTo('fast', 0)   
+        
+    });
+
+});
+
